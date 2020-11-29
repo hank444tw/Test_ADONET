8a03fd476ed31bbf7f15677f80901571bd7bab55
MSI-GL65-9SCK\hankh <10861113@gm.nfu.edu.tw>
2020-11-29 22:39:55 +0800

    加入using指示詞

--- Test_ADONET/Program.cs
+++ Test_ADONET/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.Data; //引用ADO.NET基礎物件 ex:DataSet
+//using System.Data.OleDb; //引用OLE DB資料來源物件 ex:Access,Excel,SQL Server7.0以上
+using System.Data.SqlClient; //引用SQL Server資料來源物件
 
 namespace Test_ADONET
 {
