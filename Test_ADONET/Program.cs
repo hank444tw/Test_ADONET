15176296fff7aa866c1200d35a62ad06371ff326
MSI-GL65-9SCK\hankh <10861113@gm.nfu.edu.tw>
2020-11-29 23:04:10 +0800

    使用SqlConnect物件連線test1資料庫

--- Test_ADONET/Program.cs
+++ Test_ADONET/Program.cs
@@ -14,7 +14,23 @@ namespace Test_ADONET
     {
         static void Main(string[] args)
         {
+            string cnStr = "Server=localhost;Database=test1;Trusted_Connection=True;"; //建立連接字串
+            SqlConnection cn = new SqlConnection(cnStr); //建立資料庫連接物件
+            cn.Open(); //開啟與資料庫的連線
+            
+            if(cn.State == ConnectionState.Open)
+            {
+                Console.WriteLine($"{cn.Database} 資料庫已連線"); //顯示資料庫名稱
+            }
 
+            cn.Close(); //關閉與資料庫的連線
+
+            if (cn.State == ConnectionState.Closed)
+            {
+                Console.WriteLine($"{cn.Database} 資料連線已關閉"); //顯示資料庫名稱
+            }
+
+            Console.Read();
         }
     }
 }
